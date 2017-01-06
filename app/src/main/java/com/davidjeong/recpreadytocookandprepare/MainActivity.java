867745db98ddee9c8236f7c121f365ed3fad2cbd
yechanjeong <yechanjeong@yahoo.com>
2017-01-06 13:08:18 +0100

    Checked for savedInstanceState to handle rotation

--- app/src/main/java/com/davidjeong/recpreadytocookandprepare/MainActivity.java
+++ app/src/main/java/com/davidjeong/recpreadytocookandprepare/MainActivity.java
@@ -13,13 +13,17 @@ public class MainActivity extends AppCompatActivity implements ListFragment.OnRe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment fragment = new ListFragment();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ListFragment savedFragment = (ListFragment) getFragmentManager().findFragmentById(R.id.placeHolder);
 
-        fragmentTransaction.add(R.id.placeHolder, fragment);
+        if (savedFragment == null) {
+            ListFragment fragment = new ListFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        fragmentTransaction.commit();
+            fragmentTransaction.add(R.id.placeHolder, fragment);
+
+            fragmentTransaction.commit();
+        }
     }
 
     @Override
