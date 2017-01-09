f4a8668404acb545ff14a0630b5f099b4fec8b05
yechanjeong <yechanjeong@yahoo.com>
2017-01-09 13:47:13 +0100

    Successfully put in the ingredients list with checkboxes dynamically. Working on making it scrollable

--- app/src/main/java/com/davidjeong/recpreadytocookandprepare/ViewPagerFragment.java
+++ app/src/main/java/com/davidjeong/recpreadytocookandprepare/ViewPagerFragment.java
@@ -4,6 +4,7 @@ package com.davidjeong.recpreadytocookandprepare;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -24,6 +25,10 @@ public class ViewPagerFragment extends Fragment {
         final IngredientsFragment ingredientsFragment = new IngredientsFragment();
         final DirectionsFragment directionsFragment = new DirectionsFragment();
 
+        ingredientsFragment.setArguments(getArguments());
+        directionsFragment.setArguments(getArguments());
+
+
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
         viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
             @Override
@@ -32,10 +37,18 @@ public class ViewPagerFragment extends Fragment {
             }
 
             @Override
+            public CharSequence getPageTitle(int position) {
+                return position == 0 ? "Ingredients" : "Directions";
+            }
+
+            @Override
             public int getCount() {
                 return 2;
             }
         });
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
 
         return view;
     }
